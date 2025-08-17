b215ea1c96ca13c86c1ac1be2b21534c7a0258e5
heito <me@heito.xyz>
2025-08-17 21:47:23 +0300

    The Button component has been improved, and a new Input component has been added

---
+++ dev/App.vue
@@ -0,0 +1,62 @@
+<template>
+    <main>
+        <Button variant="ghost">
+            <!-- <template #before>-</template> -->
+
+            Test Button
+            
+            <!-- <template #after>+</template> -->
+        </Button>
+
+        <div class="br"></div>
+
+        {{ text }}
+
+        <Input type="text" placeholder="A?" v-model:value="text"
+            :min="5"
+            :max="10000"
+        >
+            <!-- <template #before>123</template> -->
+            <!-- <template #after>+</template> -->
+        </Input>
+
+        <div class="br"></div>
+        
+        <Select/>
+    </main>
+</template>
+
+<script setup>
+
+import { ref } from 'vue';
+
+import { Button, Input, Select } from '../src/components';
+
+
+const text = ref('');
+
+</script>
+
+<style lang="css">
+
+body {
+    margin: 0;
+    padding: 0;
+}
+
+* {
+    color: var(--text-primary);
+}
+
+main {
+    padding: 24px;
+    height: 100vh;
+    background-color: black;
+    box-sizing: border-box;
+}
+
+.br {
+    margin: 24px;
+}
+
+</style>
---
+++ dev/main.js
@@ -0,0 +1,9 @@
+import { createApp } from 'vue';
+
+import App from './App.vue';
+
+// * Styles
+import '../src/assets/styles/vars.css';
+
+
+const app = createApp(App).mount('#app');
