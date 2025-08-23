c5c5a399d382c345ed1349e03c03e8d0a83a7a2f
heito <me@heito.xyz>
2025-08-23 23:36:32 +0300

    Hotfixed: imports ui components

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,11 +2,15 @@ import Button from './ui/Button.vue';
 import Input from './ui/Input.vue';
 import Select from './ui/Select.vue';
 import Popover from './ui/Popover.vue';
+import Dialog from './ui/Dialog.vue';
+import Tooltip from './ui/Tooltip.vue';
 
 
 export {
     Button,
     Input,
     Select,
-    Popover
+    Popover,
+    Dialog,
+    Tooltip
 }
